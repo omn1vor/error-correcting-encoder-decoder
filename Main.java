8dd6d03a9deb2563ee86073ec9c59f5450b6949b
Daniel Pimenov <daniil.pimenov@gmail.com>
2022-01-27 20:23:30 +0400

    Do the bit-wise version

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
 package correcter;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        String path = "Error Correcting Encoder-Decoder/task/src/correcter/";
+        String path = "";
         for (String arg : args) {
             if ("-local".equals(arg)) {
-                path = "";
+                path = "Error Correcting Encoder-Decoder/task/src/correcter/";
                 break;
             }
         }
@@ -33,36 +32,10 @@ public class Main {
         }
     }
 
-    static byte[] byteToBits(byte b) {
-        final byte zero = 0;
-        byte[] bits = new byte[8];
-        Arrays.fill(bits, zero);
-        int i = bits.length - 1;
-        while (b != 0 && i >= 0) {
-            if (b % 2 != 0) {
-                bits[i] = 1;
-            }
-            i--;
-            b >>>= 1;
-        }
-        return bits;
-    }
-
-    static byte bitsToByte(byte[] bits) {
-        byte value = 0;
-        final int lastIndex = bits.length - 1;
-        for (int i = lastIndex; i >= 0; i--) {
-            value += bits[i] * Math.pow(2, lastIndex - i);
-        }
-        return value;
-    }
-
     static byte addNoise(byte byteToChange) {
         final Random rnd = new Random();
         int bitToChange = rnd.nextInt(8);
-        byte[] bits = byteToBits(byteToChange);
-        bits[bitToChange] = (byte) (bits[bitToChange] == 0 ? 1 : 0);
-        return bitsToByte(bits);
+        return (byte) (byteToChange ^ (1 << bitToChange));
     }
 
 }
